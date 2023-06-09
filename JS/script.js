3c5185a5aff8c0cc08437f5095a9f7d34b121bd1
stiva98 <samustiva9810@gmail.com>
2023-06-09 16:19:55 +0200

    il mio primo JS

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const firstName = prompt ('Qual è il tuo nome?');
+const lastName = prompt ('Qual è il tuo cognome?');
+const Color = prompt ('Qual è il tuo colore preferito?');
+
+
+document.writeln ('SamueleStivalettiNero101')
